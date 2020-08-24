6852492e23274b57079f01f67937f7ebe0f4d901
Paweł Załęcki <pawelzalecki@gmail.com>
2020-08-24 19:27:13 +0200

    Adding Updating Index

--- BlazingPizza.Client/OrderState.cs
+++ BlazingPizza.Client/OrderState.cs
@@ -1,7 +1,51 @@
-﻿namespace BlazingPizza.Client
+﻿using System.Collections.Generic;
+
+namespace BlazingPizza.Client
 {
     public class OrderState
     {
-        
+        public Pizza ConfiguringPizza { get; private set; }
+        public bool ShowingConfigureDialog { get; private set; }
+        public Order Order { get; private set; } = new Order();
+
+
+        public void ConfirmConfigurePizzaDialog()
+        {
+            Order.Pizzas.Add(ConfiguringPizza);
+            ConfiguringPizza = null;
+
+            ShowingConfigureDialog = false;
+        }
+
+        public void CancelConfigurePizzaDialog()
+        {
+            ConfiguringPizza = null;
+            ShowingConfigureDialog = false;
+        }
+
+        public void RemoveConfiguredPizza(Pizza pizza)
+        {
+            Order.Pizzas.Remove(pizza);
+        }
+
+        public void ShowConfigurePizzaDialog(PizzaSpecial special)
+        {
+            ConfiguringPizza = new Pizza()
+            {
+                Special = special,
+                SpecialId = special.Id,
+                Size = Pizza.DefaultSize,
+                Toppings = new List<PizzaTopping>(),
+            };
+
+            ShowingConfigureDialog = true;
+        }
+
+        public void ResetOrder()
+        {
+            Order = new Order();
+        }
+
+
     }
 }
